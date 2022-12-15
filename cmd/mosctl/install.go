327b435c92a437fca38a43dd96a633c0c41358e1
Serge Hallyn <serge@hallyn.com>
2022-12-14 22:05:01 -0600

    create a mos type and install fn

--- cmd/mosctl/install.go
+++ cmd/mosctl/install.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/apex/log"
 	"github.com/project-machine/mos/pkg/mosconfig"
@@ -25,11 +26,6 @@ var installCmd = cli.Command{
 			Value: "/config",
 		},
 		cli.StringFlag{
-			Name: "scratchwrites-dir, s",
-			Usage: "Directory where overlay scratch writes should be mounted",
-			Value: "/scratch-writes",
-		},
-		cli.StringFlag{
 			Name: "atomfs-store, a",
 			Usage: "Directory under which atomfs store is kept",
 			Value: "/atomfs-store",
@@ -47,23 +43,27 @@ func PathExists(d string) bool {
 
 func doInstall(ctx *cli.Context) error {
 	// Expect config, scratch-writes, and atomfs-store to exist
-	if !PathExists(ctx.String("atomfs-store")) {
+	store := ctx.String("atomfs-store")
+	if !PathExists(store) {
 		return fmt.Errorf("atomfs store not found")
 	}
 
-	if !PathExists(ctx.String("config-dir")) {
-		return fmt.Errorf("mos config not found")
+	config := ctx.String("config-dir")
+	if !PathExists(config) {
+		return fmt.Errorf("mos config directory not found")
 	}
 
-	if !PathExists(ctx.String("scratchwrites-dir")) {
-		return fmt.Errorf("Scratchwrites directory not found")
-	}
-
-	cf, err := mosconfig.NewInstallFile(ctx.String("file"))
+	cpath := ctx.String("file")
+	cf, err := mosconfig.NewInstallFile(cpath)
 	if err != nil {
 		return err
 	}
 	log.Infof("Installing: %#v\n", cf)
+
+	err = mosconfig.InitializeMos(cf, store, config, filepath.Dir(cpath))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
--- tests/install.bats
+++ tests/install.bats
@@ -4,7 +4,7 @@ ROOT_DIR=$(run_git rev-parse --show-toplevel)
 
 function setup() {
 	export TMPD=$(mktemp -d "${PWD}/batstest-XXXXX")
-	mkdir -p $TMPD/config $TMPD/atomfs $TMPD/scratch
+	mkdir -p $TMPD/config $TMPD/atomfs
 }
 
 function teardown() {
@@ -19,8 +19,13 @@ function teardown() {
 version: 1
 product: de6c82c5-2e01-4c92-949b-a6545d30fc06
 targets:
+  - layer: docker://zothub.io/c3/ubuntu/base-amd64:jammy
+    name: hostfs
+    service_type: hostfs
+    nsgroup: ""
+    mounts: []
 EOF
-	./mosctl install -c $TMPD/config -a $TMPD/atomfs -s $TMPD/scratch -f $TMPD/install.yaml
+	./mosctl install -c $TMPD/config -a $TMPD/atomfs -f $TMPD/install.yaml
 }
 
 @test "mos install with bad version" {
@@ -30,6 +35,6 @@ product: de6c82c5-2e01-4c92-949b-a6545d30fc06
 targets:
 EOF
 	failed=0
-	./mosctl install -c $TMPD/config -a $TMPD/atomfs -s $TMPD/scratch -f $TMPD/install.yaml || failed=1
+	./mosctl install -c $TMPD/config -a $TMPD/atomfs -f $TMPD/install.yaml || failed=1
 	[ $failed -eq 1 ]
 }
