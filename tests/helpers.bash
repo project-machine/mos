1725acb42dc6ddcb0f617b216cd07385c02b404c
Serge Hallyn <serge@hallyn.com>
2023-03-01 16:52:37 -0600

    allow subuids to access container dirs

--- tests/helpers.bash
+++ tests/helpers.bash
@@ -36,9 +36,7 @@ function lxc_setup {
 		lxc-destroy -n mos-test-1 -f
 	}
 	lxc-copy -n mos-test -N mos-test-1
-	lxc-start -n mos-test-1 -l trace -o xxx || true
-	cat xxx
-	lxc-start -n mos-test-1
+	lxc-start -n mos-test-1 -l trace -o lxc-log || { cat lxc-log; exit 1; }
 	lxc-wait --timeout=60 -n mos-test-1 -s RUNNING
 }
 
