29e778e0e66375fe9771f3e6699daab0f38c3d97
Scott Moser <smoser@brickies.net>
2023-03-01 17:22:37 -0500

    Set up lxc.conf, lxc test changes
    Basically:
     * Write a lxc.conf for the running user so they can execute
       unpriv lxc.
     * Put a timeout on 'lxc-wait' calls
     * Remove one use of expect, replacing it with '-y' to
       add-apt-repository.
     * make tests/create-test-container.bash run with '-e' so
       it fails fast.

--- tests/create-test-container.bash
+++ tests/create-test-container.bash
@@ -1,23 +1,16 @@
 #!/bin/bash
 
+set -ex
 lxc-create -t download -n mos-test -- -d ubuntu -r jammy -a amd64
 echo "lxc.mount.entry = /dev/fuse dev/fuse none bind,create=file 0 0" >> ~/.local/share/lxc/mos-test/config
 echo "lxc.include = /usr/share/lxc/config/nesting.conf" >>  ~/.local/share/lxc/mos-test/config
 echo "lxc.apparmor.allow_nesting = 1" >>   ~/.local/share/lxc/mos-test/config
 lxc-start -n mos-test
-lxc-wait -n mos-test -s RUNNING
+lxc-wait --timeout=60 -n mos-test -s RUNNING
 lxc-attach -n mos-test -- apt-get update
 lxc-attach -n mos-test -- apt-get -y dist-upgrade
 lxc-attach -n mos-test -- apt-get -y install software-properties-common
-
-expect << "EOF"
-spawn lxc-attach -n mos-test
-expect "root@mos-test"
-send "add-apt-repository ppa:puzzleos/dev\n"
-expect "ENTER"
-send "\n"
-expect "root@mos-test"
-EOF
+lxc-attach -n mos-test -- add-apt-repository -y ppa:puzzleos/dev
 
 lxc-attach -n mos-test -- apt-get update
 lxc-attach -n mos-test -- apt-get -y install squashfuse libsquashfs1 libgpgme11 git lxc1 ubuntu-release-upgrader-core
--- tests/helpers.bash
+++ tests/helpers.bash
@@ -37,7 +37,7 @@ function lxc_setup {
 	}
 	lxc-copy -n mos-test -N mos-test-1
 	lxc-start -n mos-test-1
-	lxc-wait -n mos-test-1 -s RUNNING
+	lxc-wait --timeout=60 -n mos-test-1 -s RUNNING
 }
 
 function zot_setup {
