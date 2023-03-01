7e5ac82d160457d95d80362c0dea0f6de70ba439
Serge Hallyn <serge@hallyn.com>
2023-03-01 17:20:55 -0600

    debug

--- tests/create-test-container.bash
+++ tests/create-test-container.bash
@@ -5,7 +5,8 @@ lxc-create -t download -n mos-test -- -d ubuntu -r jammy -a amd64
 echo "lxc.mount.entry = /dev/fuse dev/fuse none bind,create=file 0 0" >> ~/.local/share/lxc/mos-test/config
 echo "lxc.include = /usr/share/lxc/config/nesting.conf" >>  ~/.local/share/lxc/mos-test/config
 echo "lxc.apparmor.allow_nesting = 1" >>   ~/.local/share/lxc/mos-test/config
-lxc-start -n mos-test
+lxc-start -n mos-test -l trace -o lxc-log.$$
+cat lxc-log.$$
 lxc-wait --timeout=60 -n mos-test -s RUNNING
 lxc-attach -n mos-test -- apt-get update
 lxc-attach -n mos-test -- apt-get -y dist-upgrade
